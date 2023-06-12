3bf3346cf260d7e2a2fd35198a7badda8511195c
Vinipcsl <viniciuspcsl@gmail.com>
2023-06-12 13:10:39 -0300

    Passo 22 concluido, consegui entender muita coisa depois de umas dicas do Anderson

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,9 +3,5 @@ sap.ui.define([
  ], function (Controller) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-       onShowHello : function () {
-          // show a native JavaScript alert
-          alert("Olá Mundo!");
-       }
     });
  });
